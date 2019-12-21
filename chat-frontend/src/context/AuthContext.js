2384d293b0d115c515b94cf4c8201911aca043d7
stoyangarov <stoyan.garov@yahoo.com>
2019-12-21 20:22:47 +0200

    Update state on signup

--- chat-frontend/src/context/AuthContext.js
+++ chat-frontend/src/context/AuthContext.js
@@ -2,16 +2,26 @@ import createDataContext from './createDataContext';
 import chatApi from '../api/chat';
 
 const authReducer = (state, action) => {
-
+  switch (action.type) {
+    case 'signin':
+      return { token: action.payload };
+    default: 
+      return state;
+  }
 };
 
 const signup = dispatch => async ({ email, password }) => {
-  const response = await chatApi.post('/signup', { email, password });
-  console.log(response.data);
+  try {
+    const response = await chatApi.post('/signup', { email, password });
+    dispatch({ type: 'signin', payload: response.data.token });
+  } catch (err) {
+    console.log(err);
+  }
+  
 };
 
 export const { Context, Provider } = createDataContext(
   authReducer,
   { signup },
-  { }
+  { token: null }
 );

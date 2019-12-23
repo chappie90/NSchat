84d1657ac30e3f27846802f284b7636bdc6b4102
stoyangarov <stoyan.garov@yahoo.com>
2019-12-23 13:15:05 +0200

    Add gifted chat to chat detail

--- chat-frontend/src/screens/ChatDetailScreen.js
+++ chat-frontend/src/screens/ChatDetailScreen.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import RemoveSocketIoWarning from '../components/RemoveSocketIoWarning';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import io from 'socket.io-client';
+import { GiftedChat } from 'react-native-gifted-chat';
 // import { AsyncStorage } from 'react-native';
 
 const ChatDetailScreen = () => {
+ const [incomingMsgs, setIncomingMsgs] = useState([]);
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = io('http://192.168.1.108:3001');
+    socket.current.on('message', message => {
+      setIncomingMsgs(prevState => GiftedChat.append(prevState, message));
+    });
+  });
+
+  const sendMessage = (messages) => {
+    socket.current.emit('message', messages[0].text);
+    setIncomingMsgs(prevState => GiftedChat.append(prevState, messages));
+  };
 
   return (
-    <View>
-      <Text>Chat Detail</Text>
+    <View style={styles.container}>
+      <GiftedChat
+        renderUsernameOnMessage 
+        messages={incomingMsgs} 
+        onSend={sendMessage} 
+        user={{ _id: 1 }} />
     </View>
   );
 };
@@ -18,7 +39,9 @@ ChatDetailScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1
+  }
 });
 
 export default ChatDetailScreen;
--- chat-frontend/src/screens/ChatsListScreen.js
+++ chat-frontend/src/screens/ChatsListScreen.js
@@ -1,34 +1,12 @@
-import RemoveSocketIoWarning from '../components/RemoveSocketIoWarning';
-import React, { useEffect, useState, useRef } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { AsyncStorage } from 'react-native';
-import io from 'socket.io-client';
-import { GiftedChat } from 'react-native-gifted-chat';
 
 const ChatsListScreen = ({ navigation }) => {
-  const [incomingMsgs, setIncomingMsgs] = useState([]);
-  const socket = useRef(null);
-
-  useEffect(() => {
-    socket.current = io('http://192.168.1.108:3001');
-    socket.current.on('message', message => {
-      setIncomingMsgs(prevState => GiftedChat.append(prevState, message));
-    });
-  });
-
-  const sendMessage = (messages) => {
-    socket.current.emit('message', messages[0].text);
-    setIncomingMsgs(prevState => GiftedChat.append(prevState, messages));
-  };
 
   return (
     <View style={styles.container}>
-    <GiftedChat
-          renderUsernameOnMessage 
-          messages={incomingMsgs} 
-          onSend={sendMessage} 
-          user={{ _id: 1 }} />
       <TouchableOpacity onPress={() => navigation.navigate('ChatDetail')}>
         <Text>Chats List</Text>
       </TouchableOpacity>
@@ -37,10 +15,8 @@ const ChatsListScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  cotainer: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center'
+  container: {
+
   }
 });
 

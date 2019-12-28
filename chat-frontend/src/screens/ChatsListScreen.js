7540bde50893930ed09676c7ef326f460ded3dda
stoyangarov <stoyan.garov@yahoo.com>
2019-12-28 12:25:01 +0000

    Style chat list items

--- chat-frontend/src/screens/ChatsListScreen.js
+++ chat-frontend/src/screens/ChatsListScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { AsyncStorage } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import Moment from 'moment';
 
 import { Context as AuthContext } from '../context/AuthContext';
 import { Context as ChatContext } from '../context/ChatContext';
@@ -15,19 +16,6 @@ const ChatsListScreen = ({ navigation }) => {
     getChats({ username });
   }, []);
 
-  const list = [
-    {
-      name: 'Amy Farha',
-      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-      subtitle: 'Vice President'
-    },
-    {
-      name: 'Chris Jackson',
-      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-      subtitle: 'Vice Chairman'
-    },
-  ]
-
   return (
     <View style={styles.container}>
       <Text style={styles.header}>My Chats</Text>   
@@ -36,8 +24,12 @@ const ChatsListScreen = ({ navigation }) => {
           <TouchableOpacity key={i} onPress={() => navigation.navigate('ChatDetail')}>
             <ListItem
               key={i}
-              // leftAvatar={{ source: { uri: l.avatar_url } }}
-              title={c.contact}
+              leftAvatar={{ source: require('../../assets/avatar2.png') }}
+              title={
+                <View style={styles.itemContainer}>
+                  <Text style={styles.name}>{c.contact}</Text><Text>{Moment(c.date).format('d MMM HH:mm')}</Text>
+                </View>
+              }
               subtitle={c.text}
               bottomDivider
               chevron
@@ -58,6 +50,13 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     marginTop: 15,
     paddingLeft: 10
+  },
+  name: {
+    fontWeight: 'bold'
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
 

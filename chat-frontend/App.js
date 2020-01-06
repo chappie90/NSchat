29a2f83718101701fe4fc40237bbb2407b0b96a7
stoyangarov <stoyan.garov@yahoo.com>
2020-01-06 23:34:46 +0000

    Load custom fonts

--- chat-frontend/App.js
+++ chat-frontend/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import ChatNavigator from './src/navigation/ChatNavigator';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { Provider as ChatProvider } from './src/context/ChatContext'; 
 import { setNavigator } from './src/components/navigationRef';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <ChatProvider>
       <AuthProvider>

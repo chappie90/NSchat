d8e8386411471bacfa12a71dd3f9ac548723e7e1
stoyangarov <stoyan.garov@yahoo.com>
2020-01-17 16:53:52 +0000

    Add basic client side input validation

--- chat-frontend/src/components/AuthForm.js
+++ chat-frontend/src/components/AuthForm.js
@@ -9,13 +9,28 @@ import {
 import { withNavigation } from 'react-navigation';
 
 import HeadingText from '../components/HeadingText';
+import BodyText from '../components/BodyText';
 import PrimaryButton from '../components/PrimaryButton';
 
 const AuthForm = ({ header, submitBtn, navLink, routeName, onSubmit, navigation, resetForm }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameIsValid, setUsernameIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
 
   let secondTextInput;
+
+  const usernameChanged = text => {
+    text.trim().length === 0 ? setUsernameIsValid(false) : setUsernameIsValid(true);
+  
+    setUsername(text); 
+  };
+
+  const passwordChanged = text => {
+    text.trim().length < 8 ? setPasswordIsValid(false) : setPasswordIsValid(true);
+
+    setPassword(text);
+  };
 
   return (
     <View style={styles.container}>
@@ -24,20 +39,23 @@ const AuthForm = ({ header, submitBtn, navLink, routeName, onSubmit, navigation,
         style={styles.input} 
         placeholder="Username"
         value={username}
-        onChangeText={setUsername}
+        onChangeText={usernameChanged}
         returnKeyType="next"
         onSubmitEditing={() => { secondTextInput.focus(); }} 
         autoCapitalize="none"
+        // blurOnSubmit={false}
         autoCorrect={false} />
+      {!usernameIsValid && <BodyText style={styles.inputError}>Username can't be an empty value</BodyText>}
       <TextInput
         style={styles.input} 
         placeholder="Password" 
         value={password}
-        onChangeText={setPassword}
+        onChangeText={passwordChanged}
         autoCorrect={false}
         autoCapitalize="none"
         secureTextEntry
         ref={(input) => { secondTextInput = input; }} />
+      {!passwordIsValid && <BodyText style={styles.inputError}>Password must be at least 8 characters long</BodyText>}
       <PrimaryButton style={styles.button} onPress={() => onSubmit({ username, password })}>
         {submitBtn}
       </PrimaryButton>
@@ -63,6 +81,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderBottomWidth: 2,
     borderBottomColor: 'lightgrey'
+  },
+  inputError: {
+    color: 'red',
+    fontSize: 16
   },
   button: {
     alignSelf: 'center',

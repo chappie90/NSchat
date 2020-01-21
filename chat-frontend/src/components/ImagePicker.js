d2b7fc99b0667231b469c7369906388962b87b5f
stoyangarov <stoyan.garov@yahoo.com>
2020-01-21 21:33:35 +0000

    Add overlay with image edit options on profile image click

--- chat-frontend/src/components/ImagePicker.js
+++ chat-frontend/src/components/ImagePicker.js
@@ -1,17 +1,19 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { View, TouchableWithoutFeedback, Text, StyleSheet, Alert } from 'react-native';
-import { Image } from 'react-native-elements';
+import { Image, Tooltip, Overlay } from 'react-native-elements';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, Ionicons, AntDesign } from '@expo/vector-icons';
 
 import Colors from '../constants/colors';
 import PrimaryButton from './PrimaryButton';
 import BodyText from './BodyText';
+import HeadingText from './HeadingText';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const ImgPicker = props => {
   const {state: { username, profileImage }, saveImage, getImage} = useContext(AuthContext);
+  const [overlayMode, setOverlayMode] = useState(false);
 
   useEffect(() => {
     getImage(username);
@@ -50,19 +52,47 @@ const ImgPicker = props => {
 
   return (
     <View style={styles.imagePickerContainer}>
-      <TouchableWithoutFeedback onPress={imageSelected}>
-        <View style={styles.profileContainer}>
-          <View style={styles.imagePreview}>
-            {profileImage ?
-              <Image resizeMode="stretch" source={{ uri: profileImage}} style={styles.image} /> : 
-              <Image source={require('../../assets/avatar2.png')} style={styles.image} />
-            }
+      <View style={styles.profileContainer}>
+        <TouchableWithoutFeedback onPress={() => setOverlayMode(true)}>
+          <View>
+            <View style={styles.imagePreview}>
+              {profileImage ?
+                <Image 
+                  placeholderStyle={styles.placeholder}
+                  source={{ uri: profileImage}}
+                  style={styles.image} /> : 
+                <Image source={require('../../assets/avatar2.png')} style={styles.image} />
+              }
+            </View>
+            <Overlay
+              isVisible={overlayMode}
+              width="auto"
+              height="auto"
+              onBackdropPress={() => setOverlayMode(false)}>
+                <View style={styles.overlayContainer}>
+                  <View style={styles.overlayItem}>
+                    <MaterialIcons color="grey" name="camera-alt" size={28} />
+                    <HeadingText style={styles.overlayText}>Take Photo</HeadingText>
+                  </View>
+                  <View style={styles.overlayItem}>
+                    <Ionicons color="grey" name="md-images" size={28} />
+                    <HeadingText style={styles.overlayText}>Choose Photo</HeadingText>
+                  </View>
+                  <View style={styles.overlayItem}>
+                    <AntDesign color={Colors.tertiary} name="delete" size={28} />
+                    <HeadingText style={styles.overlayDelete}>Delete Photo</HeadingText>
+                  </View>
+                  <View style={styles.cancel}>
+                    <HeadingText style={styles.overlayText}>Cancel</HeadingText>
+                  </View>
+                </View>
+            </Overlay>
+            <View style={styles.cameraIconContainer}>
+              <MaterialIcons style={styles.cameraIcon} name="camera-alt" size={36} />
+            </View>
           </View>
-          <View style={styles.cameraIconContainer}>
-            <MaterialIcons style={styles.cameraIcon} name="camera-alt" size={36} />
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 };
@@ -72,16 +102,19 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   profileContainer: {
-    marginTop: 30
+    marginTop: 30,
+    marginBottom: 30,
   },
   imagePreview: {
     width: 200,
     height: 200,
-    marginBottom: 30,
     borderRadius: 100,
     borderWidth: 4,
     borderColor: 'white',
     overflow: 'hidden'
+  },
+  placeholder: {
+    backgroundColor: 'white'
   },
   image: {
     width: 200,
@@ -93,9 +126,34 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'white',
     position: 'absolute',
-    top: '65%',
-    right: 15,
+    top: '68%',
+    right: 10,
     padding: 5
+  },
+  overlayContainer: {
+    padding: 15,
+  },
+  overlayItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 5,
+    marginBottom: 8,
+    borderBottomColor: 'lightgrey',
+    borderBottomWidth: 1 
+  },
+  overlayText: {
+    fontSize: 18,
+    marginLeft: 5,
+    color: 'grey'
+  },
+  overlayDelete: {
+    fontSize: 18,
+    marginLeft: 5,
+    color: Colors.tertiary
+  },
+  cancel: {
+    marginTop: 20,
+    alignSelf: 'center',
   }
 });
 

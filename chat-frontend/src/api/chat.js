fd368b696428c119012db8db8ba0b695e35d6378
stoyangarov <stoyan.garov@yahoo.com>
2020-01-26 10:33:15 +0000

    Create axios interceptor which appends user token

--- chat-frontend/src/api/chat.js
+++ chat-frontend/src/api/chat.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
   baseURL: 'http://192.168.1.174:3000'
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    let data = await AsyncStorage.getItem('data');
+    data = JSON.parse(data);
+
+    if (data && data.token) {
+      config.headers.Authorization = `Bearer ${data.token}`; 
+    }
+    return config;
+  }, 
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
 export default instance;

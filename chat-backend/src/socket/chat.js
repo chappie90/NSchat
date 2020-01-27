83e18a4db9171d3b278cf4c0fa85b5943031c163
stoyangarov <stoyan.garov@yahoo.com>
2020-01-27 19:11:08 +0000

    Emit to all contacts if online and get all online contacts on connect

--- chat-backend/src/socket/chat.js
+++ chat-backend/src/socket/chat.js
@@ -19,10 +19,8 @@ module.exports = function(io) {
   // every socket is automatically connected to their own room with the id socket.id 
   // but we are creating our own to have more control
   socket.join(username);
-
   // show list of all rooms
   // console.log(io.sockets.adapter.rooms);
-
   try {
     const user = await User.findOne({ username }, { 'contacts.username': 1, _id: 0 });
     if (!user) {
@@ -32,9 +30,7 @@ module.exports = function(io) {
     // console.log(username);
     // console.log(socketId);
     // console.log(contacts);
-
     // console.log(`${onlineContacts} before`);
-
     for (const c of contacts) {
       for (let [key, value] of Object.entries(users)) {
         if (c === key) {
@@ -56,8 +52,12 @@ module.exports = function(io) {
     // });
     // need to send yourself array of online contacts and
     // send all clients in the room your name so they can add update their state and add you in their array
-
-    io.sockets.in(username).emit('online', onlineContacts);
+    // send to everyone in the room including the sender
+    //io.sockets.in(username).emit('online', onlineContacts);
+    // send to everyone in the room except the sender
+    socket.broadcast.to(username).emit('online', username);
+    // send to sender only
+    socket.emit('online', onlineContacts);
   } catch (err) {
     console.log(err);
     return res.status(422).send({ error: 'Something went wrong with your request' });
@@ -126,6 +126,11 @@ module.exports = function(io) {
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
+    socket.broadcast.to(username).emit('offline', username);
+    io.of('/').in(username).clients((error, socketIds) => {
+      if (error) throw error;
+      socketIds.forEach(socketId => io.sockets.sockets[socketId].leave(username));
+    });
     delete users[username];
   });
 });

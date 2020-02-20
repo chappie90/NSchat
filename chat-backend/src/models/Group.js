a58bc1158e88e63e17a2ea7899d8ea7e0350edb3
stoyangarov <stoyan.garov@yahoo.com>
2020-02-20 23:44:13 +0000

    Update group model

--- chat-backend/src/models/Group.js
+++ chat-backend/src/models/Group.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
+  type: { type: String, required: true },
   name: { type: String, required: true },
   owner: { type: String, required: true },
   members: [String],

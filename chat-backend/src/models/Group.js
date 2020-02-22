e950abc5cd61379a962361dbf1142ff4dcb96a29
stoyangarov <stoyan.garov@yahoo.com>
2020-02-22 15:43:28 +0000

    Update group model naming

--- chat-backend/src/models/Group.js
+++ chat-backend/src/models/Group.js
@@ -4,7 +4,7 @@ const groupSchema = new mongoose.Schema({
   type: { type: String, default: 'group' },
   name: { type: String, required: true },
   owner: { type: String, required: true },
-  members: [String],
+  participants: [String],
   created: { type: Date, required: true, default: Date.now() },
   avatar: {
     imagePath: String,

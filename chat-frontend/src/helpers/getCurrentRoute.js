7c4c537e1a3696c4d0da4d99bb3075785450e0ed
stoyangarov <stoyan.garov@yahoo.com>
2020-05-13 22:55:51 +0100

    Create helper function to get current route

---
+++ chat-frontend/src/helpers/getCurrentRoute.js
@@ -0,0 +1,7 @@
+export const getActiveRouteState = route => {
+  if (!route.routes || route.routes.length === 0 || route.index >= route.routes.length) {
+    return route;
+  }
+  const childActiveRoute = route.routes[route.index];
+  return getActiveRouteState(childActiveRoute);
+}
